3cbbdd49e7306fb7c714a119d1d8338b007c5070
Denis1982 <d.shevchuk82@gmail.com>
2020-04-29 15:33:51 +0300

    New function game_core_v3. Binary search

--- module_0.py
+++ module_0.py
@@ -1,30 +1,36 @@
 import numpy as np
 
-def game_core_v2(number):
-    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток'''
+def game_core_v3(number):
+    '''Двоичный поиск - в каждой итерации массив делится на две части
+    , сравниваем срединное значение с искомым. Принимаем решение
+    , что может находиться только в левой или только в правой части массива 
+    (оказалось больше или меньше границы раздела). Отсекаем ту часть
+    , в которой искомого значения быть не может'''
     count = 0
-    predict = np.random.randint(1,100)
-    while number != predict:
-        count+=1
-        if number > predict: 
-            predict += 1
-        elif number < predict: 
-            predict -= 1
+    range_start = 0
+    range_finish = 100
+    while True:
+        count += 1
+        range_middle = (range_start + range_finish)//2
+        if number > range_middle:
+            range_start = range_middle
+        elif number < range_middle:
+            range_finish = range_middle
+        else:
+            break
     return(count) # выход из цикла, если угадали
-
-	
-def score_game(game_core_v2):
+        
+    
+def score_game(game_core_v3):
     '''Запускаем игру 1000 раз, чтоб узнать как быстро игра угадывает число'''
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
     random_array = np.random.randint(100, size=(1000))
     for number in random_array:
-        count_ls.append(game_core_v2(number))
+        count_ls.append(game_core_v3(number))
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return(score)
 
-	
-score_game(game_core_v2)
-	
+
+score_game(game_core_v3)
